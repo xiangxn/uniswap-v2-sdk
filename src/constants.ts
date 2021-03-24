71b6b3d5205e8e768292eecb3ae87fd19244502a
necklace <xiangxn@163.com>
2021-03-24 14:23:35 +0800

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xddea251acde34e05372cb2f1830e4f12e2022381'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x4dc202e7102add782883956e91a59c07bb328b95aec453c5c484ecbc49f225bf'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
